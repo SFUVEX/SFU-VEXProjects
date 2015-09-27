d680d549665fcf2e653545f8d741fccc3d430767
Abram Wiebe <abramw@sfu.ca>
2015-09-26 22:54:19 -0700

    Added input.h for named axis on controller.

---
+++ Template/include/input.h
@@ -0,0 +1,24 @@
+/*
+ * input.h
+ *
+ *  Created on: Sep 26, 2015
+ *      Author: awiebe
+ */
+
+#ifndef INPUT_H_
+#define INPUT_H_
+
+typedef enum InputChannels
+{
+	INPUT_RIGHT_X=1,
+	INPUT_RIGHT_Y=2,
+	INPUT_LEFT_Y=3,
+	INPUT_LEFT_X=4,
+	INPUT_RIGHT_TRIGGER=5,
+	INPUT_LEFT_TRIGGER=6,
+	INPUT_LEFT_BUTTONS=7,
+	INPUT_RIGHT_BUTTONS = 8
+
+}InputChannels;
+
+#endif /* INPUT_H_ */
